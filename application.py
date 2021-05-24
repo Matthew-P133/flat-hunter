9d69d87cca0a5696beb5bef2c1c5c643573dd73f
= <matthew.peters.work@gmail.com>
2021-05-24 16:20:48 +0100

    added delay between requests

--- application.py
+++ application.py
@@ -1,29 +1,39 @@
 from helpers import scrape, url_generator
+from time import sleep
 
+# TO DO: dynamically pass in attributes
+# attributes for testing
 attributes = {
     "minBedrooms": 5,
     "maxBedrooms": 7,
     "floorplan": 1,
 }
+
+# variable to count properties
 i = 1
 
+# generate URL based on attributes
 url = url_generator(attributes)
+
+# scrape page + update property counter
 result = scrape(url, i)
-page_dict = result[0]
 i = result[1]
 
+# add index of next page to attributes
+page_dict = result[0]
 index = page_dict["searchParameters"]["index"]
 pagination = page_dict["pagination"]
-print(pagination["next"])
 attributes["index"] =  pagination["next"]
 
+# until last page generate url of next page and scrape
 while (pagination["next"] <= pagination["last"]):
     url = url_generator(attributes)
+    sleep(5)
     result = scrape(url, i)
-    page_dict = result[0]
     i = result[1]
+
+    page_dict = result[0]
     pagination = page_dict["pagination"]
     if "next" not in pagination.keys():
         break
-    print(pagination["next"])
     attributes["index"] =  pagination["next"]
