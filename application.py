b5be73285fee9e66504dedba06c433c82d7db42f
= <matthew.peters.work@gmail.com>
2021-09-12 23:17:43 +0100

    tidied up

--- application.py
+++ application.py
@@ -1,126 +1,16 @@
 from requests import NullHandler
 from helpers import search
-from helpers import convert_html_to_pdf
+from helpers import hunt
+from helpers import scheduled_hunt
 import helpers
 from time import sleep, time
-from datetime import datetime
 from flask import Flask, render_template, request, redirect
 import sqlite3
 import json
 import schedule
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.encoders import encode_base64
-from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
-# connect to db
-db = sqlite3.connect("properties.db", check_same_thread=False)
 
 app = Flask(__name__)
-
-
-class Pdf():
-    def render_pdf(self, name, html):
-        from xhtml2pdf import pisa
-        pdf = "testfile"
-        pisa.CreatePDF(html, pdf)
-        return pdf.getvalue()
-
-
-def hunt(attributes):
-    
-    search(attributes)
-
-    cursor = db.execute("SELECT MAX(search_id) FROM properties")
-    max_search_id = cursor.fetchone()
-    print(max_search_id[0])
-    
-    properties = []
-    db.row_factory = sqlite3.Row
-    cursor = db.execute("SELECT * FROM properties where search_id=?", [max_search_id[0]])
-    
-    for row in cursor:
-        properties.append(row)
-
-    images = []
-
-    for property in properties:
-        cursor = db.execute("SELECT * FROM images")
-        for row in cursor:
-            images.append(row)
-    
-    html = render_template("results.html", properties=properties, images=images)
-    return html
-
-
-def scheduled_hunt(attributes):
-
-    search(attributes)
-
-    cursor = db.execute("SELECT MAX(search_id) FROM properties")
-    max_search_id = cursor.fetchone()
-    print(max_search_id[0])
-    
-    properties = []
-    db.row_factory = sqlite3.Row
-    cursor = db.execute("SELECT * FROM properties where search_id=?", [max_search_id[0]])
-    
-    for row in cursor:
-        properties.append(row)
-
-    images = []
-
-    for property in properties:
-        cursor = db.execute("SELECT * FROM images")
-        for row in cursor:
-            images.append(row)
-    
-    html = render_template("results_for_email.html", properties=properties, images=images)
-    
-    outputfile = f"FlatHunter-results-{datetime.now()}.pdf"
-    convert_html_to_pdf(html, outputfile)
-    email_results(outputfile)
-
-
-def email_results(outputfile):
-
-    # configure details
-    sender_address = config["address"]
-    sender_pass = config["pass"]
-    receiver_address = config["recipient"]
-
-    # content
-    mail_content = "Flats coming soon!"
-
-    # Set up the email message
-    message = MIMEMultipart()
-    message['From'] = sender_address
-    message['To'] = receiver_address
-    message['Subject'] = 'Your latest FlatHunter results'
-
-    # Configure attachment
-    
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open(f"{outputfile}", "rb").read())
-    encode_base64(part)
-    part.add_header('Content-Disposition', f'attachment; filename="{outputfile}"')
-
-    # put together email
-    message.attach(MIMEText(mail_content, 'plain'))
-    message.attach(part)
-
-    #Create SMTP session and send the email
-    session = smtplib.SMTP('smtp.gmail.com', 587)
-    session.starttls()
-    session.login(sender_address, sender_pass) 
-    text = message.as_string()
-    session.sendmail(sender_address, receiver_address, text)
-    session.quit()
-    print('Mail Sent')
 
 
 @app.route("/")
@@ -157,7 +47,6 @@ def status():
 @app.route("/results", methods=['GET', 'POST'])
 
 def results():
-
     # default attributes
     attributes = {
         "minBedrooms": 0,
--- helpers.py
+++ helpers.py
@@ -6,8 +6,20 @@ from PIL import Image
 import os
 from time import sleep
 from random import random
+from datetime import datetime
 from xhtml2pdf import pisa
- 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+from dotenv import dotenv_values
+from flask import render_template
+
+# environment variables for email username and password
+config = dotenv_values(".env")
+
+
 # initialise counter variables
 i = 1
 counter = 0
@@ -23,6 +35,7 @@ def search(attributes):
     counter = 0
     last = "[calculating]"
 
+    # connect to database
     db = sqlite3.connect("properties.db")
 
     # set search id
@@ -186,3 +199,109 @@ def convert_html_to_pdf(html, output_file):
     pisa_status = pisa.CreatePDF(html, dest=result_file)
     result_file.close()
     return pisa_status.err
+
+
+def email_results(outputfile):
+
+    # configure details
+    sender_address = config["address"]
+    sender_pass = config["pass"]
+    receiver_address = config["recipient"]
+
+    # content
+    mail_content = "Flats coming soon!"
+
+    # Set up the email message
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Your latest FlatHunter results'
+
+    # Configure attachment
+    
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(f"{outputfile}", "rb").read())
+    encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename="{outputfile}"')
+
+    # put together email
+    message.attach(MIMEText(mail_content, 'plain'))
+    message.attach(part)
+
+    #Create SMTP session and send the email
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(sender_address, sender_pass) 
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+
+
+def hunt(attributes):
+    
+    search(attributes)
+
+    # connect to database
+    db = sqlite3.connect("properties.db")
+
+    cursor = db.execute("SELECT MAX(search_id) FROM properties")
+    max_search_id = cursor.fetchone()
+    print(max_search_id[0])
+    
+    properties = []
+    db.row_factory = sqlite3.Row
+    cursor = db.execute("SELECT * FROM properties where search_id=?", [max_search_id[0]])
+    
+    for row in cursor:
+        properties.append(row)
+
+    images = []
+
+    for property in properties:
+        cursor = db.execute("SELECT * FROM images")
+        for row in cursor:
+            images.append(row)
+    
+    html = render_template("results.html", properties=properties, images=images)
+    return html
+
+
+def scheduled_hunt(attributes):
+
+    search(attributes)
+
+    # connect to database
+    db = sqlite3.connect("properties.db")
+
+    cursor = db.execute("SELECT MAX(search_id) FROM properties")
+    max_search_id = cursor.fetchone()
+    print(max_search_id[0])
+    
+    properties = []
+    db.row_factory = sqlite3.Row
+    cursor = db.execute("SELECT * FROM properties where search_id=?", [max_search_id[0]])
+    
+    for row in cursor:
+        properties.append(row)
+
+    images = []
+
+    for property in properties:
+        cursor = db.execute("SELECT * FROM images")
+        for row in cursor:
+            images.append(row)
+    
+    html = render_template("results_for_email.html", properties=properties, images=images)
+    
+    outputfile = f"FlatHunter-results-{datetime.now()}.pdf"
+    convert_html_to_pdf(html, outputfile)
+    email_results(outputfile)
+
+
+    class Pdf():
+        def render_pdf(self, name, html):
+            from xhtml2pdf import pisa
+            pdf = "testfile"
+            pisa.CreatePDF(html, pdf)
+            return pdf.getvalue()
